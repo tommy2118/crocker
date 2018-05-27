26f2735c96e993f1d142a55c67022728e2510c73
Tommy A. Caruso <tommy.caruso2118@gmail.com>
2018-05-27 14:58:15 -0400

    Add validation and association tests to Recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,28 @@ class Recipe < ApplicationRecord
   has_many :steps
   has_many :components
   has_many :ingredients, through: :components
+
+  validates :name, presence: true
+
+  enum protein: [
+    :beef,
+    :chicken,
+    :pork,
+    :other
+  ]
+
+  enum cook_method: [
+    :oven,
+    :grill,
+    :stove_top,
+    :crock_pot
+  ]
+
+  enum diet_type: [
+    :traditional,
+    :low_carb,
+    :vegan,
+    :vegetarian
+  ]
+
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  it { is_expected.to have_many(:steps) }
-  it { is_expected.to have_many(:ingredients).through(:components) }
+  context "associations" do
+    it { is_expected.to have_many(:steps) }
+    it { is_expected.to have_many(:components) }
+    it { is_expected.to have_many(:ingredients).through(:components) }
+  end
+
+  context "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to define_enum_for(:protein)
+      .with([:beef, :chicken, :pork, :other])}
+    it { is_expected.to define_enum_for(:cook_method)
+      .with([:oven, :grill, :stove_top, :crock_pot])}
+    it { is_expected.to define_enum_for(:diet_type)
+      .with([:traditional, :low_carb, :vegan, :vegetarian])}
+  end
 end
